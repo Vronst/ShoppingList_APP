dc6fca49af9bc57541ba26ba5f808329639d119f
Vronst <vronst@gmail.com>
2023-11-28 20:11:51 +0100

    put and get works

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import requests
 
 
+# function that will sort our dicts based on zone and then priority
 def sorting(thing):
     return thing['zone'], thing['priority']
 
@@ -15,22 +16,24 @@ class ApiConnection:
     # function to add product to data offline
     def add_data(self, name, price, zone, priority, quantity, taken):
         new = {
-                    'name': name,
-                    'price': price,
-                    'zone': zone,
-                    'priority': priority,
-                    'quantity': quantity,
-                    'taken': taken
-                }
+            "list": {
+                'name': name,
+                'price': price,
+                'zone': zone,
+                'priority': priority,
+                'quantity': quantity,
+                'taken': taken
+            }
+        }
         # checking name in every index of list to see if there is our new element
         # if not appending to list
         for x in range(len(self.data['list'])):
-            if new['name'] != self.data['list'][x]['name']:
-                if x == len(self.data['list'])-1:
-                    self.data['list'].append(new)
-                    with open('data.json', 'w') as file:
-                        json.dump(self.data, fp=file, indent=4)
-                    requests.post(url=self.api, json=self.data)
+            if new['list']['name'] != self.data['list'][x]['name']:
+                if x == len(self.data['list']) - 1:
+                    # self.data['list'].append(new['list'])
+                    msg = requests.post(url=self.api, json=new)
+                    self.synch_data()
+                    print(msg)
             else:
                 break
 
@@ -38,16 +41,25 @@ class ApiConnection:
     def synch_data(self):
         self.data = requests.get(url=f'{self.api}').json()
 
-    def update(self):
-        update_data = []
+    def update_taken(self):
         # gets only data with taken = '1'
         # (to not wipe out progress of another person using same app / sheets)
         for part in self.data['list']:
-            if part['taken'] == '1':
-                update_data.append(part)
-        print(update_data)
-        # TODO: how to update online and note wipe out data
+            if part['taken'] == 1:
+                self.update(part)
 
+    def update_all(self):
+        for part in self.data['list']:
+            self.update(part)
+
+    def update(self, part):
+        upd = {"list": {
+            'name': part['name'],
+            'price': part['price'],
+            'zone': part['zone'],
+            'priority': part['priority'],
+            'quantity': part['quantity'],
+            'taken': part['taken']
+        }}
+        requests.put(url=f'{self.api}/{part["id"]}', json=upd)
     # pushing data online
-
-    # function that will sort our dicts based on zone and then priority
